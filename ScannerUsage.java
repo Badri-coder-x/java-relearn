adf911c88f2dccbef348ef4082427f20282a4f13
Badri-coder-x <101438112+Badri-coder-x@users.noreply.github.com>
2025-06-14 13:40:48 +0530

    init commit new file to learn Scanner class

---
+++ ScannerUsage.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ScannerUsage {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //read a line
+        sc.nextLine();
+        //read a word
+        sc.next();
+        //read an int
+        sc.nextInt();
+        //read a double
+        sc.nextDouble();
+        //read a boolean
+        sc.nextBoolean();
+
+    }
+}
