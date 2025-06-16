1ccb9c86905d6d29237e584c4cafa9d86389613e
Badri-coder-x <101438112+Badri-coder-x@users.noreply.github.com>
2025-06-16 19:55:06 +0530

    init commit created an Operator checking program

---
+++ OperatorsInJava.java
@@ -0,0 +1,11 @@
+public class OperatorsInJava {
+
+    public static void main(String[] args){
+        //Arithmetic Operators in Java
+
+        int result = 3 + 4 * (7-5)/2;
+        System.out.println(result);
+
+
+    }
+}
