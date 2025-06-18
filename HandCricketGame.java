f103d6be0b52b894880a7b45570fb014228ee9c1
Badri-coder-x <101438112+Badri-coder-x@users.noreply.github.com>
2025-06-18 21:05:45 +0530

    I am going to commit this half-baked code, I will add further changes in the future

--- HandCricketGame.java
+++ HandCricketGame.java
@@ -95,6 +95,139 @@ public class HandCricketGame {
 
 
             }
+            // finishing of a part...
+            else{
+                System.out.print("Enter whether you want first batting or bowling: ");
+                String input2 = sc.nextLine();
+
+                if(input2.toLowerCase()=="batting"){
+                    int stopFlag2 = 11, userRun= 0;
+                    System.out.println();
+                    while(true){
+
+                        System.out.print("Enter the value: ");
+                        userRun=sc.nextInt();
+                        stopFlag2 = random.nextInt(1,11);
+
+                        if(stopFlag2 != userRun){
+                            playerScore+=userRun;
+                            System.out.println("- - - - Scoreboard - - - -");
+                            System.out.println("Computer tried "+stopFlag2);
+                            System.out.println("User scored "+userRun+" runs...");
+                            System.out.println("User's total score: "+playerScore);
+                        }
+                        else {
+                            if(playerScore<compScore) {
+                                System.out.println("- - - - Scoreboard - - - -");
+                                System.out.println("Computer tried "+stopFlag2);
+                                System.out.println("User's total score: " + playerScore);
+                                System.out.println("Wicket!!!! The player has to failed to score the target. Computer wins !!!");
+                                break;
+                            }
+                            else if (playerScore==compScore){
+                                System.out.println("- - - - Scoreboard - - - -");
+                                System.out.println("Computer tried "+stopFlag2);
+                                System.out.println("User's total score: " + playerScore);
+                                System.out.println("Wicket!!!! The match is draw");
+                                break;
+                            }
+                            else {
+                                System.out.println("- - - - Scoreboard - - - -");
+                                System.out.println("Computer tried "+stopFlag2);
+                                System.out.println("User's total score: " + playerScore);
+                                System.out.println("User won the match");
+                                break;
+                            }
+                        }
+                    }
+
+
+                    System.out.println();
+                    int stopFlag = 11, compRun= 0;
+                    while(true){
+
+                        compRun = random.nextInt(1,11);
+                        System.out.print("Enter the value: ");
+                        stopFlag=sc.nextInt();
+
+                        if(stopFlag != compRun){
+                            compScore+=compRun;
+                            System.out.println("- - - - Scoreboard - - - -");
+                            System.out.println("Computer scored "+compRun+" runs...");
+                            System.out.println("Computer's total score: "+compScore);
+                        }
+                        else {
+                            System.out.println("- - - - Scoreboard - - - -");
+                            System.out.println("Computer: "+compScore);
+                            System.out.println("Wicket!!!! now player has to bat and choose the target "+compScore);
+                            break;
+                        }
+                    }
+                }
+                else{
+                    System.out.println("You chose to bowl first !!!");
+                    int stopFlag = 11, compRun= 0;
+                    while(true){
+
+                        compRun = random.nextInt(1,11);
+                        System.out.print("Enter the value: ");
+                        stopFlag=sc.nextInt();
+
+                        if(stopFlag != compRun){
+                            compScore+=compRun;
+                            System.out.println("- - - - Scoreboard - - - -");
+                            System.out.println("Computer scored "+compRun+" runs...");
+                            System.out.println("Computer's total score: "+compScore);
+                        }
+                        else {
+                            System.out.println("- - - - Scoreboard - - - -");
+                            System.out.println("Computer: "+compScore);
+                            System.out.println("Wicket!!!! now player has to bat and choose the target "+compScore);
+                            break;
+                        }
+                    }
+
+                    int stopFlag2 = 11, userRun= 0;
+                    System.out.println();
+                    while(true){
+
+                        System.out.print("Enter the value: ");
+                        userRun=sc.nextInt();
+                        stopFlag2 = random.nextInt(1,11);
+
+                        if(stopFlag2 != userRun){
+                            playerScore+=userRun;
+                            System.out.println("- - - - Scoreboard - - - -");
+                            System.out.println("Computer tried "+stopFlag2);
+                            System.out.println("User scored "+userRun+" runs...");
+                            System.out.println("User's total score: "+playerScore);
+                        }
+                        else {
+                            if(playerScore<compScore) {
+                                System.out.println("- - - - Scoreboard - - - -");
+                                System.out.println("Computer tried "+stopFlag2);
+                                System.out.println("User's total score: " + playerScore);
+                                System.out.println("Wicket!!!! The player has to failed to score the target. Computer wins !!!");
+                                break;
+                            }
+                            else if (playerScore==compScore){
+                                System.out.println("- - - - Scoreboard - - - -");
+                                System.out.println("Computer tried "+stopFlag2);
+                                System.out.println("User's total score: " + playerScore);
+                                System.out.println("Wicket!!!! The match is draw");
+                                break;
+                            }
+                            else {
+                                System.out.println("- - - - Scoreboard - - - -");
+                                System.out.println("Computer tried "+stopFlag2);
+                                System.out.println("User's total score: " + playerScore);
+                                System.out.println("User won the match");
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
 
 
 
